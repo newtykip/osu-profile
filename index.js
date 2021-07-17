73b546326da306838bc2acab8b7b4b0ac115b5c7
newt <newtykins@outlook.com>
2021-07-17 16:28:10 +0100

    Add replaceAll to prototype

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ String.prototype.capitalise = function () {
 	return `${this.charAt(0).toUpperCase()}${this.slice(1)}`;
 }
 
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+}
+
 const generateRegex = tag => new RegExp(`(?<=(${tag}))(.*?)(?=\s*(${tag}))`);
 const replaceTags = (tags, value, ascii) => {
 	let response = ascii;

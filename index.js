478599c29b55793d45af87a226842851fbb83e65
newt <newtykins@outlook.com>
2021-07-17 15:11:15 +0100

    Attempt to get the readme

--- index.js
+++ index.js
@@ -9,11 +9,6 @@ try {
 	const owner = process.env.GITHUB_REPOSITORY.split('/')[0];
 	const repo = process.env.GITHUB_REPOSITORY.split('/')[1]; 
 	console.log(process.env.GITHUB_REPOSITORY, owner, repo);
-	
-	octokit.rest.repos.get({ owner, repo })
-		.then(({ data }) => {
-			console.log(data.url)
-		});
 
 	// Request the user's profile information
 	https.get(`https://about.newtt.me/api/osu/${id}`, (resp) => {
@@ -25,7 +20,29 @@ try {
 		resp.on('end', () => {
 			const res = JSON.parse(data);
 			const rank = res.globalRank.toLocaleString();
-			
+
+			// Get the readme
+			octokit.rest.repos.getReadme({
+				owner,
+				repo,
+			})
+				.then(({ data: readme }) => {
+					console.log(readme.path, readme.content);
+
+					/*
+					// Update the readme
+					octokit.rest.repos.createOrUpdateFileContents({
+						owner,
+						repo,
+						path: readme.path,
+						sha: readme.sha,
+						message: 'Updated osu! rank',
+						committer: {
+							name: 'osu-rank-bot',
+							email: '41898282+github-actions[bot]@users.noreply.github.com'
+						}
+					})*/
+				})
 		});
 	});
 } catch (error) {

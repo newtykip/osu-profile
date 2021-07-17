2357945d6232dd77126caad23a52e991b99271ff
newt <newtykins@outlook.com>
2021-07-17 16:06:14 +0100

    Add all new templates to cover entire API

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const https = require('https');
+const countries = require('i18n-iso-countries');
+const moment = require('moment');
 
 const generateRegex = tag => new RegExp(`(?<=(${tag}))(.*?)(?=\s*(${tag}))`);
-const replacePlaceholder = (placeholder, value, ascii) => ascii.replace(generateRegex(placeholder), value);
+const replacePlaceholder = (placeholders, value, ascii) => {
+	let response = ascii;
+
+	placeholders.forEach(placeholder => {
+		response = ascii.replace(generateRegex(placeholder), value);	
+	});
+
+	return response;
+}
 
 try {
 	// Take from input
@@ -26,7 +36,6 @@ try {
 		// Parse it
 		resp.on('end', () => {
 			const res = JSON.parse(data);
-			const rank = `#${res.globalRank.toLocaleString()}`;
 
 			// Get the readme from the repository
 			octokit.rest.repos.getReadme({
@@ -39,8 +48,23 @@ try {
 					let readmeASCII = readmeBuffer.toString('ascii');
 
 					// Update the ASCII, replacing all placeholders supported by the action
-					readmeASCII = replacePlaceholder('<!--osu-rank-->', rank, readmeASCII);
-					readmeASCII = replacePlaceholder('<!--osu-id-->', res.id, readmeASCII);
+					readmeASCII = replacePlaceholder(['<!--osu-name-->', '<!--osu-username-->'], res.username, readmeASCII);					
+					readmeASCII = replacePlaceholder(['<!--osu-id-->'], res.id, readmeASCII);
+					readmeASCII = replacePlaceholder(['<!--osu-rank-->', '<!--osu-global-rank-->'], `#${res.globalRank.toLocaleString()}`, readmeASCII);
+					readmeASCII = replacePlaceholder(['<!--osu-country-rank-->'], `#${res.countryRank.toLocaleString()}`, readmeASCII);					
+					readmeASCII = replacePlaceholder(['<!--osu-country-->'], countries.getName(res.country), readmeASCII);
+					readmeASCII = replacePlaceholder(['<!--osu-pp-->'], res.pp, readmeASCII);
+					readmeASCII = replacePlaceholder(['<!--osu-level-->'], Math.floor(res.level), readmeASCII);
+					readmeASCII = replacePlaceholder(['<!--osu-time-->'], res.timePlayed, readmeASCII);
+					readmeASCII = replacePlaceholder(['<!--osu-accuracy-->'], res.accuracy, readmeASCII);
+					readmeASCII = replacePlaceholder(['<!--osu-avatar-->'], res.avatar, readmeASCII);
+					readmeASCII = replacePlaceholder(['<!--osu-join-date-->'], moment(res.joinDate).format('ddd, MMM Do, YYYY h:mm A'), readmeASCII);
+					readmeASCII = replacePlaceholder(['<!--osu-playcount-->'], res.playCount.toLocaleString(), readmeASCII);
+					readmeASCII = replacePlaceholder(['<!--osu-ranked-score-->'], res.scores.ranked.toLocaleString(), readmeASCII);
+					readmeASCII = replacePlaceholder(['<!--osu-total-score-->'], res.scores.total.toLocaleString(), readmeASCII);
+					readmeASCII = replacePlaceholder(['<!--osu-ranks-ss-->'], res.ranks.ss.total.toLocaleString(), readmeASCII);
+					readmeASCII = replacePlaceholder(['<!--osu-ranks-s-->'], res.ranks.s.total.toLocaleString(), readmeASCII);
+					readmeASCII = replacePlaceholder(['<!--osu-ranks-a-->'], res.ranks.a.total.toLocaleString(), readmeASCII);
 
 					// Convert the ASCII back into base64
 					const contentBuffer = Buffer.from(readmeASCII, 'ascii');
--- readme.md
+++ readme.md
@@ -2,16 +2,6 @@
 
 This action exposes placeholders for you to include information about your osu! account on your GitHub profile readme.
 
-## Placeholders
-
-### \<!--osu-id-->\<!--osu-id-->
-
-Gets replaced with your osu profile ID!
-
-#### \<!--osu-rank-->\<!--osu-rank-->
-
-Gets replaced with your global rank on osu!
-
 ## Inputs
 
 ### ID
@@ -25,3 +15,73 @@ uses: newtykins/osu-profile@main
 with:
 	ID: '16009610'
 ```
+
+## Placeholders
+
+### \<!--osu-name-->\<!--osu-name--> or \<!--osu-username-->\<!--osu-username-->
+
+Gets replaced with your username!
+
+### \<!--osu-id-->\<!--osu-id-->
+
+Gets replaced with your profile ID!
+
+### \<!--osu-rank-->\<!--osu-rank--> or \<!--osu-global-rank-->\<!--osu-global-rank--> 
+
+Gets replaced with your global rank!
+
+### \<!--osu-country-rank-->\<!--osu-country-rank-->
+
+Gets replaced with your rank in the country!
+
+### \<!--osu-country-->\<!--osu-country-->
+
+Gets replaced with your country!
+
+### \<!--osu-pp-->\<!--osu-pp-->
+
+Gets replaced with your total pp!
+
+### \<!--osu-level-->\<!--osu-level-->
+
+Gets replaced with your level!
+
+### \<!--osu-time-->\<!--osu-time-->
+
+Gets replaced with the time you have played for!
+
+### \<!--osu-accuracy-->\<!--osu-accuracy-->
+
+Gets replaced with your overall account accuracy!
+
+### \<!--osu-avatar-->\<!--osu-avatar-->
+
+Gets replaced with a URL to your account's avatar!
+
+### \<!--osu-join-date-->\<!--osu-join-date-->
+
+Gets replaced with your join date!
+
+### \<!--osu-playcount-->\<!--osu-playcount-->
+
+Gets replaced with your playcount!
+
+### \<!--osu-ranked-score-->\<!--osu-ranked-score-->
+
+Gets replaced with your ranked score!
+
+### \<!--osu-total-score-->\<!--osu-total-score-->
+
+Gets replaced with your total score!
+
+### \<!--osu-ranks-ss-->\<!--osu-ranks-ss-->
+
+Gets replaced with the amount of SSes you have!
+
+### \<!--osu-ranks-s-->\<!--osu-ranks-s-->
+
+Gets replaced with the amount of S ranks you have!
+
+### \<!--osu-ranks-a-->\<!--osu-ranks-a-->
+
+Gets replaced with the amount of A ranks you have!

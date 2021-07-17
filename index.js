b6582822c5a75f09431d68fb028980d06f172ead
newt <newtykins@outlook.com>
2021-07-17 17:59:39 +0100

    Allow for emojis

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const github = require('@actions/github');
 const https = require('https');
 const moment = require('moment');
 
-const replaceTag = (tag, value, ascii) => {
+const replaceTag = (tag, value, unicode) => {
 	const regex = new RegExp(`(?<=(<!--osu-${tag}-->))(.*?)(?=\s*(<!--osu-${tag}-->))`, 'g');
 	console.log(tag, value);
-	return ascii.replace(regex, value);
+	return unicode.replace(regex, value);
 }
 
 try {
@@ -38,32 +38,32 @@ try {
 				repo,
 			})
 				.then(({ data: readme }) => {
-					// Read the content of the readme and convert it to ASCII
+					// Read the content of the readme and convert it to Unicode
 					const readmeBuffer = Buffer.from(readme.content, 'base64');
-					let readmeASCII = readmeBuffer.toString('ascii');
+					let readmeUnicode = readmeBuffer.toString('utf-8');
 
-					// Update the ASCII, replacing all placeholders supported by the action
-					readmeASCII = replaceTag('username', res.username, readmeASCII);					
-					readmeASCII = replaceTag('id', parseInt(res.id), readmeASCII);
-					readmeASCII = replaceTag('global-rank', `#${parseInt(res.globalRank).toLocaleString()}`, readmeASCII);
-					readmeASCII = replaceTag('country-rank', `#${parseInt(res.countryRank).toLocaleString()}`, readmeASCII);					
-					readmeASCII = replaceTag('country', res.country, readmeASCII);
-					readmeASCII = replaceTag('pp', parseInt(res.pp), readmeASCII);
-					readmeASCII = replaceTag('level', Math.floor(res.level), readmeASCII);
-					readmeASCII = replaceTag('time', res.timePlayed, readmeASCII);
-					readmeASCII = replaceTag('accuracy', parseFloat(res.accuracy), readmeASCII);
-					readmeASCII = replaceTag('join-date', moment(res.joinDate).format('ddd, MMM Do, YYYY h:mm A'), readmeASCII);
-					readmeASCII = replaceTag('play-count', parseInt(res.playCount).toLocaleString(), readmeASCII);
-					readmeASCII = replaceTag('ranked-score', parseInt(res.scores.ranked).toLocaleString(), readmeASCII);
-					readmeASCII = replaceTag('total-score', parseInt(res.scores.total).toLocaleString(), readmeASCII);
-					readmeASCII = replaceTag('ss', parseInt(res.ranks.ss.total).toLocaleString(), readmeASCII);
-					readmeASCII = replaceTag('s', parseInt(res.ranks.s.total).toLocaleString(), readmeASCII);
-					readmeASCII = replaceTag('a', parseInt(res.ranks.a).toLocaleString(), readmeASCII);
+					// Update the Unicode, replacing all placeholders supported by the action
+					readmeUnicode = replaceTag('username', res.username, readmeUnicode);					
+					readmeUnicode = replaceTag('id', parseInt(res.id), readmeUnicode);
+					readmeUnicode = replaceTag('global-rank', `#${parseInt(res.globalRank).toLocaleString()}`, readmeUnicode);
+					readmeUnicode = replaceTag('country-rank', `#${parseInt(res.countryRank).toLocaleString()}`, readmeUnicode);					
+					readmeUnicode = replaceTag('country', res.country, readmeUnicode);
+					readmeUnicode = replaceTag('pp', parseInt(res.pp), readmeUnicode);
+					readmeUnicode = replaceTag('level', Math.floor(res.level), readmeUnicode);
+					readmeUnicode = replaceTag('time', res.timePlayed, readmeUnicode);
+					readmeUnicode = replaceTag('accuracy', parseFloat(res.accuracy), readmeUnicode);
+					readmeUnicode = replaceTag('join-date', moment(res.joinDate).format('ddd, MMM Do, YYYY h:mm A'), readmeUnicode);
+					readmeUnicode = replaceTag('play-count', parseInt(res.playCount).toLocaleString(), readmeUnicode);
+					readmeUnicode = replaceTag('ranked-score', parseInt(res.scores.ranked).toLocaleString(), readmeUnicode);
+					readmeUnicode = replaceTag('total-score', parseInt(res.scores.total).toLocaleString(), readmeUnicode);
+					readmeUnicode = replaceTag('ss', parseInt(res.ranks.ss.total).toLocaleString(), readmeUnicode);
+					readmeUnicode = replaceTag('s', parseInt(res.ranks.s.total).toLocaleString(), readmeUnicode);
+					readmeUnicode = replaceTag('a', parseInt(res.ranks.a).toLocaleString(), readmeUnicode);
 
-					console.log(readmeASCII);
+					console.log(readmeUnicode);
 
-					// Convert the ASCII back into base64
-					const contentBuffer = Buffer.from(readmeASCII, 'ascii');
+					// Convert the Unicode back into base64
+					const contentBuffer = Buffer.from(readmeUnicode, 'utf-8');
 					const content = contentBuffer.toString('base64');
 
 					// Update the readme
